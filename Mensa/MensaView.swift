9a38ce7d44cc4674b324dabfd68e1e98301cfad9
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-01-07 12:57:15 +0100

    Optional testing

--- Mensa/MensaView.swift
+++ Mensa/MensaView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import MockMealAdapters
 import MealPorts
+import OpenMensaMealAdapters
 
 struct MensaView: View {
     @StateObject var viewModel = MensaViewModel()
@@ -25,7 +26,12 @@ struct MensaView: View {
             .onAppear {
                 Task(priority: .medium) {
                     do {
-                        try success(meals: await MockGetMealsCommand().execute(inDTO: MealQueryDTO(mensa: 1, date: Date())))
+                        let meals = try await MockGetMealsCommand().execute(inDTO: MealQueryDTO(mensa: 42, date: Date()))
+/*                        let formatter = DateFormatter()
+                        formatter.dateFormat = "yyyy/MM/dd"
+                        let someDateTime = formatter.date(from: "2022/01/11")!
+                        let meals = try await GetOpenMensaMealsCommand().execute(inDTO: MealQueryDTO(mensa: 42, date: someDateTime))*/
+                        success(meals: meals)
                     } catch let error {
                         failure(error: error)
                     }
@@ -36,9 +42,19 @@ struct MensaView: View {
     func success(meals: [MealCollection]) {
         viewModel.categories = meals.map({
             collection in
-            CategoryViewModel(name: collection.name, meals: collection.meals.map({
+            var name = "Hauptgericht"
+            switch collection.category {
+            case .dessert: name = "Desserts"
+            case .none: name = ""
+            case .dish: name = "Hauptgerichte"
+            case .sidedish: name = "Beilagen"
+            case .soup: name = "Suppen / Eintöpfe"
+            }
+            return CategoryViewModel(name: name, meals: collection.meals.map({
                 meal in
-                MealViewModel(title: meal.name, subtitle: "\(meal.price.employees!)", image: URL(string: meal.image)!)
+                let price = "\(String(describing: meal.price.employees))"
+                let image = URL(string: meal.image) ?? URL(string: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=100&q=80")!
+                return MealViewModel(title: meal.name, subtitle: price, image: image)
                 
             }))
             
